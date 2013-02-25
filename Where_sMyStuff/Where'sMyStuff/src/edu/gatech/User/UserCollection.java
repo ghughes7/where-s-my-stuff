a2ba8b5deb3a0739be85baef2dcb2bccdfc2003d
Christal Sengkhamphong <cms6@gatech.edu>
2013-02-25 17:01:42 -0500

    M5 Feature Slice 1
    Configure the Android App and show an initial screen

---
+++ Where_sMyStuff/Where'sMyStuff/src/edu/gatech/User/UserCollection.java
@@ -0,0 +1,18 @@
+package edu.gatech.User;
+import java.util.*;
+
+
+public class UserCollection {
+	
+	private List<User> userArray = new ArrayList<User>();
+	
+	public UserCollection(){
+		User u = new User("fakeuser","fake","blah@gmail.com");	
+		userArray.add(u);
+	}
+	
+	public User getUser(){
+		return userArray.get(0);
+		
+	}
+}
