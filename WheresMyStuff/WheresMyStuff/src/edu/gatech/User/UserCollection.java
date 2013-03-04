c312658951a400a7f64dbe15b5e870f059371938
Christal Sengkhamphong <cms6@gatech.edu>
2013-03-04 00:42:48 -0500

    Updated Register Page

---
+++ WheresMyStuff/WheresMyStuff/src/edu/gatech/User/UserCollection.java
@@ -0,0 +1,23 @@
+package edu.gatech.User;
+import java.util.*;
+
+
+public class UserCollection {
+	
+	private User u;
+	private List<User> userArray = new ArrayList<User>();
+	
+	public UserCollection(){
+		User u = new User("fakeuser","fake","blah@gmail.com");	
+		userArray.add(u);
+	}
+	
+	public User addUser(User u){
+		return u;
+	}
+	
+	public User getUser(){
+		return userArray.get(0);	
+	}
+
+}
