8afa9d16d52fc0a5c50ad6f9ed89a24e572f9be0
galen <galenhughes7@gmail.com>
2013-03-05 14:54:57 -0500

    Cleaning up merged branches <Item-Pages> with          < master>

--- Where_sMyStuff/Where'sMyStuff/gen/edu/gatech/TriggerHappyCoders/R.java
+++ Where_sMyStuff/Where'sMyStuff/gen/edu/gatech/TriggerHappyCoders/R.java
@@ -23,7 +23,7 @@ public final class R {
         public static final int username=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
