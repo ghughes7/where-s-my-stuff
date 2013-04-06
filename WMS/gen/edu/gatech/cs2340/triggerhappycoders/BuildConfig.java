0a181ff96a1c08fa433fc9d1ab3d4a5473f24ed8
ghughes7 <galenhughes7@gmail.com>
2013-04-05 22:02:57 -0400

    Added a .gitignore file to ignore automatically generated files

--- WMS/gen/edu/gatech/cs2340/triggerhappycoders/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.gatech.cs2340.triggerhappycoders;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
