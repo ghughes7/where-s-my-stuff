cfd1e70d88a495c3bd74f89497b7b3ce5452e775
ghughes7 <galenhughes7@gmail.com>
2013-04-06 02:07:45 -0400

    Javadocs for MapFragment(work in progress)

--- WMS/src/edu/gatech/cs2340/thc/model/MapFragment.java
+++ WMS/src/edu/gatech/cs2340/thc/model/MapFragment.java
@@ -8,11 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+/**
+ * This Fragment will be responsible for displaying the item's location on
+ * Google Maps. Will interface with Google API's. -Extra Credit being worked on
+ * 
+ * @author Trigger Happy Coders
+ * @version 1.0
+ */
 public class MapFragment extends Fragment {
-
+	/**
+	 * Default Android implementation to create Fragments.
+	 */
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.activity_map_fragment, container, false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.activity_map_fragment, container,
+				false);
 	}
 
 }
