651c836c89d186f3bcfbdb37c1463eb3ee46a4c0
ghughes7 <galenhughes7@gmail.com>
2013-04-06 02:24:20 -0400

    Javadocs for SettingsFragment

--- WMS/src/edu/gatech/cs2340/thc/model/SettingsFragment.java
+++ WMS/src/edu/gatech/cs2340/thc/model/SettingsFragment.java
@@ -8,8 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+/**
+ * Settings (Extra-Credit) 
+ * Customize how you use the application
+ * 
+ * @author Trigger Happy Coders
+ * @version 1.0
+ */
 public class SettingsFragment extends Fragment {
 
+	/**
+	 * Default Android implementation of Fragments
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.activity_settings_fragment, container, false);
